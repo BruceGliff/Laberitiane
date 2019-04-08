e01a128b340c53f451d3d706b21c7fc629adb0fd
Bruce Gliff <ilyaawaA@yandex.ru>
2019-04-09 00:32:03 +0300

    new work with skins, added object player(test)

--- Laberitiane/main.cpp
+++ Laberitiane/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include "Player.h"
 #include <iostream>
 
 using namespace sf;
@@ -7,27 +8,16 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	sf::RenderWindow window(sf::VideoMode(100, 100), "Laberitianin");
+	sf::RenderWindow window(sf::VideoMode(200, 200), "Laberitianin");
 
-	RectangleShape shape(Vector2f(200, 100));
+	RectangleShape shape(Vector2f(200, 200));
 	shape.setFillColor(Color(255, 255, 255));
 
 	shape.setPosition(0, 0);
 
-	Image img;
-	img.loadFromFile("ref/images/skins.png");
-
-	Texture texture;
-	texture.loadFromImage(img);
-
-	Sprite sprite;
-	sprite.setTexture(texture);
-	sprite.setTextureRect(IntRect(0, 64, 16, 16));
-	sprite.setPosition(50, 25);
+	Player p(50, 50, 7);
 
 	Clock clock;
-
-	float CurrFrame = 0;
 
 	while (window.isOpen())
 	{
@@ -42,42 +32,35 @@ int main()
 				window.close();
 		}
 
+		//EAST
 		if (Keyboard::isKeyPressed(Keyboard::Right) || Keyboard::isKeyPressed(Keyboard::D))
 		{
-			CurrFrame += 0.035 * time;
-			if (CurrFrame > 3) CurrFrame -= 3;
-
-			sprite.setTextureRect(IntRect(16 * int(CurrFrame), 80, 16, 16));
-			sprite.move(0.1 * time, 0);
+			p.updateRect(EAST, 0.1, time);
 		}
+
+		//WEST
 		if (Keyboard::isKeyPressed(Keyboard::Left) || Keyboard::isKeyPressed(Keyboard::A))
 		{ 
-			CurrFrame += 0.035 * time;
-			if (CurrFrame > 3) CurrFrame -= 3;
-			
-			sprite.setTextureRect(IntRect(16 * (int(CurrFrame) + 1), 80, -16, 16));
-			sprite.move(-0.1 * time, 0);
+			p.updateRect(WEST, 0.1, time);
 		}
+
+		//NORTH
 		if (Keyboard::isKeyPressed(Keyboard::Up) || Keyboard::isKeyPressed(Keyboard::W)) 
 		{ 
-			CurrFrame += 0.035 * time;
-			if (CurrFrame > 3) CurrFrame -= 3;
-
-			sprite.setTextureRect(IntRect(16 * int(CurrFrame), 96, 16, 16));
-			sprite.move(0, -0.1 * time);
+			p.updateRect(NORTH, 0.1, time);
 		}
+
+		//SOUTH
 		if (Keyboard::isKeyPressed(Keyboard::Down) || Keyboard::isKeyPressed(Keyboard::S))
 		{
-			CurrFrame += 0.035 * time;
-			if (CurrFrame > 3) CurrFrame -= 3;
-
-			sprite.setTextureRect(IntRect(16 * int(CurrFrame), 64, 16, 16));
-			sprite.move(0, 0.1 * time);
+			p.updateRect(SOUTH, 0.1, time);
 		}
+		p.upload(time);
 
 		window.clear();
+
 		window.draw(shape);
-		window.draw(sprite);
+		window.draw(p.spr_);
 
 		window.display();
 	}
